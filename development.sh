4ae1b4d9a337c3eda95f9b954694e0fee921c93a
leo-do-9x <leo.do.9x@gmail.com>
2024-08-08 10:00:42 +0700

    create development.sh

--- development.sh
+++ development.sh
@@ -5,25 +5,19 @@ fancy_echo() {
   printf "\\n$fmt\\n" "$@"
 }
 
-if ! command -v brew >/dev/null; then
-   fancy_echo "Installing Homebrew"
-   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fancy_echo "Installing development cli"
+fancy_echo "--------------------------"
 
-   fancy_echo "Update path of the homebrew"
-   (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> ~/.zprofile
-   eval "$(/opt/homebrew/bin/brew shellenv)"
-
-   fancy_echo "Reinstall brew cask"
-   brew reinstall cask
-fi
-
-cli=("git" "node" "yarn")
+cli=("git" "node" "yarn" "pnpm" "composer")
 for cli in ${cli[*]}; do
     if ! brew list $cli; then
         fancy_echo "Installing $cli"
         brew install $cli
     fi
 done
+
+fancy_echo "Customizing the git"
+fancy_echo "-------------------"
 
 fancy_echo "Installing oh my zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -32,7 +26,10 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 fancy_echo "Installing zsh-autosuggestions for zsh"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-gui=("evkey" "docker" "tableplus" "brave-browser" "visual-studio-code" "font-fira-code" "slack" "notion" "the-unarchiver" "raycast"  "warp" "cron" "postman" "headset" "cold-turkey-blocker")
+fancy_echo "Installing development apps"
+fancy_echo "--------------------------"
+
+gui=("docker" "herd" "tableplus" "visual-studio-code" "font-fira-code" "slack" "warp" "postman")
 for gui in ${gui[*]}; do
     if ! brew list $gui; then
         fancy_echo "Installing $gui"
