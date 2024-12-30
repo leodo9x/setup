d8480f0c8cc9fae8d8b996211ce3e162c4485ccf
leo-do-9x <leo.do.9x@gmail.com>
2024-12-30 10:49:51 +0700

    update gui app

--- app.sh
+++ app.sh
@@ -20,7 +20,7 @@ fi
 fancy_echo "Installing essential apps"
 fancy_echo "-------------------------"
 
-gui=("evkey" "brave-browser" "notion-calendar" "the-unarchiver" "raycast" "mochi" "obsidian" "vlc" "proton-drive")
+gui=("evkey" "brave-browser" "the-unarchiver" "raycast" "obsidian" "megasync" "vlc")
 for gui in ${gui[*]}; do
     if ! brew list $gui; then
         fancy_echo "Installing $gui"
